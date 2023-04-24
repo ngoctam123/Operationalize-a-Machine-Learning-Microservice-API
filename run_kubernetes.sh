7596da586ec6c378525a9c16555cf844661841bc
ngoctam123 <ngoctam.love1999@gmail.com>
2023-04-24 13:16:37 +0700

    update

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,7 +5,7 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
-dockerpath="TamVN/mlapi"
+dockerpath="TamVN/microproject"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
--- upload_docker.sh
+++ upload_docker.sh
@@ -6,13 +6,13 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
-dockerpath="clairelee7522/mlapi"
+dockerpath="TamVN/microproject"
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
 docker login &&\
-    docker image tag mlapi $dockerpath
+    docker image tag microproject $dockerpath
 
 # Step 3:
 # Push image to a docker repository
